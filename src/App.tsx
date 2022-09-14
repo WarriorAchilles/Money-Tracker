624feb34933553ca6875741278d198c8e43eaf0e
Zion Emond <ZionEm7@gmail.com>
2022-09-14 16:48:49 -0400

    changed the basic example code to have a header and body with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Timer = () => {
+  return (
+    <div>
+      <h1>Testing 1 2 3</h1>
+      <p>Hello, I am going to be a timer</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Money Tracker</h1>
       </header>
+      <body>
+        <Timer />
+      </body>
     </div>
   );
 }
