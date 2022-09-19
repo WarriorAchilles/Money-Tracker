326735fdb2ea9762c9c1122c4f07b0afd512afb5
Zion Emond <ZionEm7@gmail.com>
2022-09-19 11:43:24 -0400

    refactored unneccesarily complicated minutes/hours calculation

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -4,8 +4,6 @@ import './Timer.css';
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
   function toggle() {
@@ -14,8 +12,6 @@ const Timer = () => {
 
   function reset() {
     setSeconds(0);
-    setMinutes(0);
-    setHours(0);
     setIsActive(false);
   }
 
@@ -23,27 +19,19 @@ const Timer = () => {
     let interval : ReturnType<typeof setInterval>;
     if (isActive) {
       interval = setInterval(() => {
-        if (seconds === 59) {
-          setSeconds(0);
-          if (minutes === 59) {
-            setMinutes(0);
-            setHours(hours => hours + 1)
-          } else {
-            setMinutes(minutes => minutes + 1);
-          }
-        } else {
-          setSeconds(seconds => seconds + 1);
-        }
+        setSeconds(seconds => seconds + 1);
       }, 1000);
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds, minutes, hours])
+  }, [isActive, seconds])
 
   return (
     <div>
       <div className="timer">
-          {hours.toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}:{minutes.toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}:{seconds.toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}
-      </div>
+      {Math.floor(seconds / 3600).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}
+      :{(Math.floor(seconds / 60) % 60).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}
+      :{(seconds % 60).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping: false})}
+    </div>
       <div>
         <button className="btn btn-primary mx-1" onClick={toggle}>
           {isActive ? 'Pause' : 'Start'}
